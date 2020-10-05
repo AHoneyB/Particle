aada697ccc15670b60ff6c56c3995a7041f7cabc
AhoneyB <ahoneyx42@gmail.com>
2020-10-06 10:33:31 +1300

    Start of particle system coding

---
+++ Displacement.js
@@ -0,0 +1,50 @@
+class Displacement {
+  constructor(mass, x, y, vx, vy) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(vx, vy);
+    this.acc = createVector(0, 0);
+    this.mass = mass;
+    this.r = sqrt(this.mass) * 10;
+  }
+
+  netForce(force) {
+    // Use static version of division
+    var f = p5.Vector.div(force, this.mass);
+    //force.div(this.mass);
+    this.acc.add(f);
+  }
+
+  boundry() {
+    if (this.pos.y > height) {
+      if (this.vel.y > 0) this.vel.y *= -1;
+    }
+    if (this.pos.y < 0) {
+      if (this.vel.y < 0) this.vel.y *= -1;
+    }
+    //  if (this.pos.y > height-this.r) DETERMINE EDGE of circle
+    // this.pos = hieght-this.r;
+
+    if (this.pos.x > width) {
+      if (this.vel.x > 0) this.vel.x *= -1;
+    }
+    if (this.pos.x < 0) {
+      if (this.vel.x < 0) this.vel.x *= -1;
+    }
+  }
+
+  update() {
+    // set last state to current state before update
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+    // set last state
+    // check boundries
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255, 100);
+    ellipse(this.pos.x, this.pos.y, 2 * this.r);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+var displacement;
+var pointMasses = [];
+const num = 200;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  const maxv = 10;
+  createCanvas(600, 600);
+  for (i = 0; i < num; i++) {
+    pointMasses[i] = new Displacement(
+      random(1, 6),
+      width / 2,
+      height / 2,
+      random(-maxv, maxv),
+      random(-maxv, maxv)
+    );
+    //pointMasses[i] = new Displacement(i + 1, width / 2, height * 0.1, 0, 0);
+  }
+  background(0);
 }
 
 function draw() {
+  background(0);
 
+  var gravity = createVector(0, 0.05);
+  if (mouseIsPressed) {
+    var wind = createVector(1, 0);
+    for (i = 0; i < num; i++) {
+      pointMasses[i].netForce(wind);
+    }
+  }
+
+  var gravity = createVector(0, 0.05);
+  // for (i = 0; i < num; i++) {
+  // 	var weight = p5.Vector.mult(gravity, pointMasses[i].mass);
+  // 	pointMasses[i].netForce(weight);
+  // }
+
+  for (i = 0; i < num; i++) {
+    pointMasses[i].update();
+    pointMasses[i].boundry();
+    pointMasses[i].show();
+  }
 }
